40ad262b19b54a13241b58f83b8a58e044d3fce5
Alec Jandorek <ajandorek@gmail.com>
2018-06-06 11:54:16 -0500

    create boilerplate react setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import './style/_main.scss';
+
+render(<App />, document.getElementById('root'));
