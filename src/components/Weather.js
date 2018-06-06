fd289dbfd003ce2ebec390a5b285176a9cc5f3ba
Alec Jandorek <ajandorek@gmail.com>
2018-06-06 16:00:11 -0500

    fix naming errors

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { currentWeather } from '../utils/current_weather';
+import { currentWeather } from '../utils/weather';
 
 export default class Weather extends Component {
   constructor() {
     super();
     this.state = {
-      data: null,
-      res: false,
-      temp: null,
+      weatherData: null,
+      apiResponse: false,
+      temperature: null,
     };
   }
 
@@ -18,18 +18,19 @@ export default class Weather extends Component {
   getWeather() {
     currentWeather().then(res =>
       this.setState({
-        data: res.data,
-        res: true,
-        temp: res.data.main.temp,
+        weatherData: res.data,
+        apiResponse: true,
+        temperature: res.data.main.temp,
       }));
   }
 
   render() {
-    if (this.state.res === false) return <div>Loading</div>;
+    const { apiResponse } = this.state;
+    if (apiResponse === false) return <div>Loading</div>;
     return (
       <div>
-        <h1>Today's weather for {this.state.data.name}</h1>
-        <h2>{this.state.temp}</h2>
+        <h1>Today's weather for {this.state.weatherData.name}</h1>
+        <h2>{this.state.temperature}</h2>
       </div>
     );
   }
--- src/utils/weather.js
+++ src/utils/weather.js
@@ -3,9 +3,11 @@ import axios from 'axios';
 export const currentWeather = async () => {
   const url = `http://api.openweathermap.org/data/2.5/weather?lat=30.267153&lon=-97.7430608&appid=${
     process.env.OPENWEATHER_APIKEY
-  }`;
+  }&units=imperial`;
 
-  const res = await axios.get(url);
+  const response = await axios.get(url);
 
-  return res;
+  return response;
 };
+
+export const toCelcius = temp => Math.round((temp - 32) * (5 / 9) * 100) / 100;
