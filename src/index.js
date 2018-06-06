87fbd1eee26a79fdeb8ac388b8d72b1b9f10fe09
Alec Jandorek <ajandorek@gmail.com>
2018-06-06 15:23:49 -0500

    create currentWeather function and render weather for austin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import './style/_main.scss';
+import '../style/_main.scss';
 
 render(<App />, document.getElementById('root'));
