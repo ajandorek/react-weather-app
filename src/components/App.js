ec45091749f20bdb5b1fab8c7a9071a5494a7ca0
Alec Jandorek <ajandorek@gmail.com>
2018-06-06 11:54:16 -0500

    create boilerplate react setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>React Boilerplate with Sass</h1>
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+      alt="react-logo"
+    />
+  </div>
+);
+
+export default App;
