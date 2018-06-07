55b2cc67194ee044f178007304311933cf9df91b
Alec Jandorek <ajandorek@gmail.com>
2018-06-07 10:32:10 -0500

    dynamically grab a users location

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,6 @@ import Weather from './Weather';
 
 const App = () => (
   <div>
-    <h1>React Boilerplate with Sass</h1>
-    <img
-      src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-      alt="react-logo"
-    />
     <Weather />
   </div>
 );
