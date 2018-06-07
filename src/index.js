3d50c226ec51789a26109fbcf82721ee65f4b3b2
Alec Jandorek <ajandorek@gmail.com>
2018-06-07 17:21:23 -0500

    create UVIndex component, add button for 3 and 5 day forecast, minor styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import '@trendmicro/react-toggle-switch/dist/react-toggle-switch.css';
 
 import App from './components/App';
 import '../node_modules/weather-icons-lite/css/weather-icons-lite.min.css';
