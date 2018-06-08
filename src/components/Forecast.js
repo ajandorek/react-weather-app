9cc488f1e2ce7b470f69d5649dcc29b2bdadf708
Alec Jandorek <ajandorek@gmail.com>
2018-06-08 16:39:28 -0500

    fix requested errors

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ForecastDay from './ForecastDay';
 
 const Forecast = props => {
-  const { apiResponse, forecastData } = props;
+  const { apiResponse, forecastData, isCelcius } = props;
   if (apiResponse === false) return <div>Loading</div>;
   return (
     <div className="forecast">
       {forecastData.list.map((day, i) => (
-        <ForecastDay key={i} weather={day} isCelcius={props.isCelcius} />
+        <ForecastDay key={i} weather={day} isCelcius={isCelcius} />
       ))}
     </div>
   );
--- src/components/UnitToggle.js
+++ src/components/UnitToggle.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ToggleSwitch from '@trendmicro/react-toggle-switch';
 
-const UnitToggle = props => (
-  <div>
-    F
-    <ToggleSwitch
-      size="sm"
-      checked={props.checkUnit}
-      onChange={() => {
-        props.unitChange();
-      }}
-    />
-    C
-  </div>
-);
+const UnitToggle = props => {
+  const { unitChange, checkUnit } = props;
+  return (
+    <div>
+      F
+      <ToggleSwitch size="sm" checked={checkUnit} onChange={unitChange} />
+      C
+    </div>
+  );
+};
 
 export default UnitToggle;
