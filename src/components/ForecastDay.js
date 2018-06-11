2eb3b030b1d16e579830e893dcff43db1070cf9d
Alec Jandorek <ajandorek@gmail.com>
2018-06-11 10:35:39 -0500

    destructure 'unix' from moment

--- src/components/ForecastDay.js
+++ src/components/ForecastDay.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import moment from 'moment';
+import { unix } from 'moment';
 import { renderWeatherImg, toCelcius } from '../utils/weather.js';
 
 const ForecastDay = props => {
   const { weather, isCelcius } = props;
   return (
     <div className="forecastday">
-      <h2>{moment.unix(weather.dt).format('ddd')}</h2>
+      <h2>{unix(weather.dt).format('ddd')}</h2>
       <h3>{isCelcius ? `${toCelcius(weather.temp.day)}° C` : `${weather.temp.day}° F`}</h3>
       <p>{weather.weather[0].main}</p>
       <i className={renderWeatherImg(weather.weather[0].icon)} />
