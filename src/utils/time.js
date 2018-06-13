02dca7d5bee58f247b84bcb205181a2baabace9d
Alec Jandorek <ajandorek@gmail.com>
2018-06-13 14:49:05 -0500

    add in storage of data in local storage for one hour before expiration

---
+++ src/utils/time.js
@@ -0,0 +1,15 @@
+import moment from 'moment';
+
+export const setTimeStamp = () => {
+  localStorage.setItem('timeStamp', moment().unix());
+};
+
+export const checkTimeStamp = () => {
+  const expirationTime = 6000;
+  const currentTimestamp = moment().unix();
+  const timeStamp = localStorage.getItem('timeStamp');
+  if (currentTimestamp - timeStamp >= expirationTime) {
+    return false;
+  }
+  return true;
+};
