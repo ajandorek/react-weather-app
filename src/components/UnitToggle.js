9f9abb5cd60f7b5beb93e6015904ab8e24ed0b9f
Alec Jandorek <ajandorek@gmail.com>
2018-06-13 13:16:53 -0500

    add styling and mobile responsiveness to the application

--- src/components/UnitToggle.js
+++ src/components/UnitToggle.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { TEMP_CONSTS } from '../utils/weather';
 
 const UnitToggle = props => {
-  const { changeUnit, temperature } = props;
+  const { changeUnit } = props;
   return (
     <div className="unitChange">
       <button className="unitChange__button" onClick={() => changeUnit(TEMP_CONSTS.FAHRENHEIT)}>
