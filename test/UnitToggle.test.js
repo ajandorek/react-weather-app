28a2ed63fe3a48c8bd0186576269a797a3a89e6b
Alec Jandorek <ajandorek@gmail.com>
2018-06-14 15:31:55 -0500

    create basic unit tests and utility files

--- test/UnitToggle.test.js
+++ test/UnitToggle.test.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import UnitToggle from '../src/components/UnitToggle';
 
 it('renders UnitToggle component without crashing', () => {
   shallow(<UnitToggle />);
+});
+
+it('should convert toFahrenheit onClick', () => {
+  const toFahrenheit = jest.fn();
+  const wrapper = mount(<UnitToggle changeUnit={toFahrenheit} />);
+
+  const button = wrapper.find('button.unitChange__button').at(0);
+  button.simulate('click');
+  expect(toFahrenheit).toHaveBeenCalled();
+});
+
+it('should convert toCelcius onClick', () => {
+  const toCelcius = jest.fn();
+  const wrapper = mount(<UnitToggle changeUnit={toCelcius} />);
+
+  const button = wrapper.find('button.unitChange__button').at(1);
+  button.simulate('click');
+  expect(toCelcius).toHaveBeenCalled();
 });
