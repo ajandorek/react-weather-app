ad44f16577a508a7a4f006ada1e4d58b1939b63a
Alec Jandorek <ajandorek@gmail.com>
2018-06-14 15:58:58 -0500

    add tests for forecast page and create mock data

---
+++ test/utils/mockForecastDay.js
@@ -0,0 +1,10 @@
+import { TEMP_CONSTS } from '../../src/utils/weather';
+
+export const data = {
+  dayOfWeek: 123412341234,
+  temperature: 95.67,
+  weather: 'Sunny',
+  icon: '01d',
+  unit: 'F',
+  type: TEMP_CONSTS.FAHRENHEIT,
+};
